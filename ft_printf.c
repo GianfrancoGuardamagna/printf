a9b379ea823c6c279591091800542ee3a473ca09
Gianfranco Guardamagna <gguardamagna23@gmail.com>
2025-05-14 14:10:35 +0200

    va encaminado

--- ft_printf.c
+++ ft_printf.c
@@ -1,36 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gguardam <gguardam@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/13 14:55:59 by gguardam          #+#    #+#             */
+/*   Updated: 2025/05/14 14:07:58 by gguardam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
 
-static int	parameter_count(const char	*stringy)
-{
-	int	i;
-	int	parameters;
+// void ft_printnbr(va_list vargs)
+// {
+// 	int	number = va_arg(vargs, int);
+// 	write(1, &number, sizeof(int));
+// }
 
-	i = 0;
-	parameters = 0;
-	while(stringy[i] != '\0')
+void ft_printchar(va_list vargs)
+{
+	char charsy = va_arg(vargs, char);
+	write(1, &charsy, sizeof(char));
+}
+
+// void ft_printstr(va_list vargs)
+// {
+	
+// }
+
+static void	parameter_manager(char	stringy, va_list vargs)
+{
+	if (stringy == 'c')
 	{
-		if (stringy[i - 1] == '%' && (stringy[i] == 'c' || stringy[i] == 's' || stringy[i] == 'p' || stringy[i] == 'd' || stringy[i] == 'i' || stringy[i] == 'u' || stringy[i] == 'x' || stringy[i] == 'X' || stringy[i] == '%'))
-			parameters++;
-		i++;
+		ft_printchar(vargs);
 	}
-	return parameters;
+	// else if (stringy == 's')
+	// {
+	// 	va_arg(vargs, char *);
+	// 	printf("%s", vargs);
+	// }
+	// else if (stringy == 'p')
+	// {
+	// 	va_arg(vargs, void *);
+	// 	printf("%p", vargs);
+	// }
+	// else if (stringy == 'd' || stringy == 'i')
+	// {
+	// 	va_arg(vargs, int);
+	// 	printf("%i", vargs);
+	// }
+	// else if (stringy == 'u')
+	// {
+	// 	va_arg(vargs, int);
+	// 	printf("%i", vargs);
+	// }
+	// else if (stringy == 'x' || stringy == 'X')
+	// {
+	// 	va_arg(vargs, int);
+	// 	printf("%i", vargs);
+	// }
+	// else if (stringy == '%')
+	// {
+	// 	va_arg(vargs, int);
+	// 	printf("%i", vargs);
+	// }
 }
 
 int	ft_printf(char const *stringy, ...)
 {
 	va_list	vargs;
-	char	*variable;
-	int	parameters;
 
 	va_start(vargs, stringy);
-	parameters = parameter_count(stringy);
-	while (parameters > 0)
+	while (*stringy != '\0')
 	{
-		variable = va_arg(vargs, char *);
-		puts(variable);
-		parameters--;
+		if (*stringy == '%')
+		{	
+			stringy++;
+			parameter_manager(*stringy, vargs);
+		}
+		stringy++;
+		write(1, stringy, sizeof(char));
 	}
 	va_end(vargs);
 	return 0;
@@ -38,6 +90,6 @@ int	ft_printf(char const *stringy, ...)
 
 int	main()
 {
-	ft_printf("Ay Miguel: %s\n", 5);
+	ft_printf("%c Ay Miguel %c\n", 'A', 'r');
 	return 0;
 }
